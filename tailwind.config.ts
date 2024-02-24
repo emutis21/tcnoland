146519894db6bc803fbe754cc7ef3befd11e086d
Esteban Mutis <smithmutis21@gmail.com>
2024-02-24 11:18:12 -0500

    Add Prettier configuration and update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'breakerbay': {
+          '50': '#f1fcfb',
+          '100': '#cef9f5',
+          '200': '#9ef1eb',
+          '300': '#65e3de',
+          '400': '#36cbc9',
+          '500': '#1a9e9f',
+          '600': '#148a8d',
+          '700': '#146e71',
+          '800': '#15575a',
+          '900': '#16494b',
+          '950': '#06292d',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
