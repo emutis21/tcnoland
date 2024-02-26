410cd337f19a1ca1d9ee86cca4d49aa65e8571ba
Esteban Mutis <smithmutis21@gmail.com>
2024-02-25 20:37:00 -0500

    Refactor page component and add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,28 @@
 import type { Metadata } from 'next'
 import { Jost } from 'next/font/google'
-import { Navbar } from '@/components/ui/Navbar'
 
 import './globals.css'
+import CartProvider from '@/modules/cart/context'
+import ProductProvider from '@/modules/product/context'
 
 const jost = Jost({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tcnoland',
-  description: 'Una página web fantástica de venta de celulares',
+  description: 'Una página web fantástica de venta de celulares'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
     <html lang='es'>
       <body className={jost.className}>
-        <Navbar />
-        {children}
+        <ProductProvider>
+          <CartProvider>{children}</CartProvider>
+        </ProductProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,47 @@
-import api from './api'
+import { Navbar } from '@/components/ui/Navbar/Navbar'
+import api from '../modules/product/api'
+import { Button } from '@/components/ui/button'
 
 export default async function Home() {
   const products = await api.list()
+
+  const brands = new Set(products.map(({ brand }) => brand))
+
+  // TODO: improve this, componentize it and add to cart
   return (
-    <main
-      // data-type='card'
-      className='flex flex-col items-center justify-between [grid-column:full-width]'
-    >
-      <ul className='grid grid-cols-1 gap-24 sm:grid-cols-2 lg:grid-cols-3'>
-        {products.map((products) => (
-          <li
-            key={products.id}
-            className='flex flex-col items-center justify-between rounded-lg px-4 py-2'
-            data-type='card'
-          >
-            <h2>
-              {products.brand} {products.model}
-            </h2>
-            <p>{products.processor}</p>
-            <p>{products.screen}</p>
-            <img
-              src={products.image}
-              alt={products.brand}
-              className='aspect-[14/16] w-64 rounded-lg object-cover'
-            />
-            <p>{products.price}</p>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <Navbar />
+
+      <Button variant='primary' size='md'>
+        Cómpralo
+      </Button>
+      <main
+        className='flex flex-col items-center justify-between [grid-column:full-width]'
+      >
+        <ul className='grid grid-cols-1 gap-24 sm:grid-cols-2 lg:grid-cols-3'>
+          {products.map(({ id, brand, model, processor, screen, image, price }) => {
+            return (
+              <li
+                key={id}
+                className='flex flex-col items-center justify-between rounded-lg px-4 py-2'
+                data-type='card'
+              >
+                <h2>
+                  {brand} {model}
+                </h2>
+                <p>{processor}</p>
+                <p>{screen}</p>
+                <img
+                  src={image}
+                  alt={brand}
+                  className='aspect-[14/16] w-64 rounded-lg object-cover'
+                />
+                <p>{price}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+    </>
   )
 }
