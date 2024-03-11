9cd49f5e9fc79e76625afd90fe6226b9ad9aab02
Esteban Mutis <smithmutis21@gmail.com>
2024-03-11 07:30:40 -0500

    Add Loading component and refactor Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 import { Navbar } from '@/components/ui/Navbar/Navbar'
-import api from '../modules/product/api'
-import { Button } from '@/components/ui/button'
+import { AsideComponent } from '@/components/ui/aside'
 import StoreScreen from '@/modules/store/screens/Store'
+import api from '../modules/product/api'
 
-export default async function Home() {
+export default async function Home({ searchParams }: { searchParams?: { query: string, brand: string } }) {
   const products = await api.list()
+  const query = searchParams?.query || ''
+  const brand = searchParams?.brand || ''
 
-  const brands = new Set(products.map(({ brand }) => brand))
-
-  // TODO: improve this, componentize it and add to cart
   return (
     <>
       <Navbar />
-
-      {/* <Button variant='primary' size='md'>
-        Cómpralo
-      </Button> */}
       <h1 className='mb-8 text-center text-4xl font-semibold [grid-column:content]'>Store</h1>
 
-      <StoreScreen products={products} />
+      <AsideComponent />
+      <StoreScreen products={products} query={query} brand={brand} />
     </>
   )
 }
