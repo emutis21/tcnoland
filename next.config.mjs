4f19455d0f1776ccb0ba25c9b25a9cba09cf7a02
Esteban Mutis <smithmutis21@gmail.com>
2024-03-29 21:26:38 -0500

    Add experimental ppr and remotePatterns to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  experimental: {
+    ppr: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.*.*',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.*',
+        pathname: '/**'
+      }
+    ]
+  }
 }
 
 export default nextConfig
