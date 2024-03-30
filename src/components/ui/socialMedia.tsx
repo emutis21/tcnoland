7263a0de693e742193b4538c332d4f3404ec3240
Esteban Mutis <smithmutis21@gmail.com>
2024-03-30 16:05:26 -0500

    Refactor social media icons positioning

--- src/components/ui/socialMedia.tsx
+++ src/components/ui/socialMedia.tsx
@@ -7,61 +7,59 @@ import { TwitterIcon } from '../icons/twitter'
 
 export function SocialMedia({ store }: { store: Store }) {
   return (
-    <div className='relative'>
-      <div className='absolute -top-[30px] left-0 flex -translate-y-1/2 transform gap-2'>
-        {store.instagram ? (
-          <a
-            aria-label='Instagram'
-            href={store.instagram}
-            rel='noopener noreferrer'
-            target='_blank'
-            title='Síguenos en Instagram'
-          >
-            <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
-              <InstagramIcon />
-            </div>
-          </a>
-        ) : null}
-        {store.whatsapp ? (
-          <a
-            aria-label='Whatsapp'
-            href={store.whatsapp}
-            rel='noopener noreferrer'
-            target='_blank'
-            title='Contáctanos por Whatsapp'
-          >
-            <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
-              <WhatsappIcon />
-            </div>
-          </a>
-        ) : null}
-        {store.facebook ? (
-          <a
-            aria-label='Facebook'
-            href={store.facebook}
-            rel='noopener noreferrer'
-            target='_blank'
-            title='Síguenos en Facebook'
-          >
-            <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
-              <FacebookIcon />
-            </div>
-          </a>
-        ) : null}
-        {store.twitter ? (
-          <a
-            aria-label='Twitter'
-            href={store.twitter}
-            rel='noopener noreferrer'
-            target='_blank'
-            title='Síguenos en Twitter'
-          >
-            <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
-              <TwitterIcon />
-            </div>
-          </a>
-        ) : null}
-      </div>
+    <div className='absolute -left-[9px] -top-12 flex p-0'>
+      {store.instagram ? (
+        <a
+          aria-label='Instagram'
+          href={store.instagram}
+          rel='noopener noreferrer'
+          target='_blank'
+          title='Síguenos en Instagram'
+        >
+          <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
+            <InstagramIcon />
+          </div>
+        </a>
+      ) : null}
+      {store.whatsapp ? (
+        <a
+          aria-label='Whatsapp'
+          href={store.whatsapp}
+          rel='noopener noreferrer'
+          target='_blank'
+          title='Contáctanos por Whatsapp'
+        >
+          <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
+            <WhatsappIcon />
+          </div>
+        </a>
+      ) : null}
+      {store.facebook ? (
+        <a
+          aria-label='Facebook'
+          href={store.facebook}
+          rel='noopener noreferrer'
+          target='_blank'
+          title='Síguenos en Facebook'
+        >
+          <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
+            <FacebookIcon />
+          </div>
+        </a>
+      ) : null}
+      {store.twitter ? (
+        <a
+          aria-label='Twitter'
+          href={store.twitter}
+          rel='noopener noreferrer'
+          target='_blank'
+          title='Síguenos en Twitter'
+        >
+          <div className='bg-brand-600 flex h-10 w-10 items-center justify-center rounded-full text-breakerbay-600 transition-colors hover:text-sky-600 dark:text-breakerbay-50 dark:hover:text-breakerbay-400'>
+            <TwitterIcon />
+          </div>
+        </a>
+      ) : null}
     </div>
   )
 }
